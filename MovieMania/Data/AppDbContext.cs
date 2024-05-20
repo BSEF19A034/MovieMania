265b9f2543ee90fa180d39688269c6d66e2eca9a
zulqa <zulqa@DESKTOP-8EQ8EPF>
2024-05-20 18:03:32 +0500

    Add project files.

---
+++ MovieMania/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieMania.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        
+        
+        { 
+        
+        
+        
+            
+        }
+
+    }
+}
+  
---
+++ MovieMania/Data/Enum/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieMania.Data.Enum
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
